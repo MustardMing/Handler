3f23655756755bd351988251bf78c0779ee54736
曹启明 <parkour@caoqimingdeMacBook-Pro.local>
2017-09-15 13:50:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/parkour/trainx/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.parkour.trainx;
+
+import android.app.Service;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import  android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "myServiceTag";
+    MyService myService = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.txtContent);
+
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                textView.setText(msg.arg1 + "");
+            }
+        };
+
+
+
+
+        handler.post(new Runnable(){
+            @Override
+            public void run() {
+                textView.setText("123");
+            }
+        });
+
+
+        final Runnable myWorker = new Runnable() {
+            @Override
+            public void run() {
+                int progress = 0;
+                while (progress <= 100) {
+                    Message msg = new Message();
+                    msg.arg1 = progress;
+                    handler.sendMessage(msg);
+                    progress += 10;
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Message msg = handler.obtainMessage();//同 new Message();
+                msg.arg1 = -1;
+                handler.sendMessage(msg);
+            }
+        };
+        Button button = (Button) findViewById(R.id.btnStart);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Thread workThread = new Thread(null, myWorker, "WorkThread");
+                workThread.start();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/parkour/trainx/MyService.java
@@ -0,0 +1,37 @@
+package com.example.parkour.trainx;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MyService extends Service {
+    private static final String TAG="myServiceTag";
+
+    //onBind返回的IBinder接口对象
+    private LocalBinder myBinder=new LocalBinder();
+
+    //计算器binder
+    public class LocalBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    public int add(int x,int y){
+        return x+y;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.v(TAG, "onBind()");
+        return myBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.v(TAG, "onUnbind()");
+        return super.onUnbind(intent);
+    }
+}
